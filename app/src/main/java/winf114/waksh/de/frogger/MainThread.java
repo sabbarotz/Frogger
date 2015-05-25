39ff112872fe0d9f6f765ebb29f37b1231d1412f
M.Findeisen <m.findeisen@solprime.de>
2015-05-26 01:14:49 +0200

    MF 25.5. added with little change, no code deleted

--- app/src/main/java/winf114/waksh/de/frogger/MainThread.java
+++ app/src/main/java/winf114/waksh/de/frogger/MainThread.java
@@ -1,6 +1,9 @@
 package winf114.waksh.de.frogger;
 
 import java.util.ArrayList;
+import android.view.SurfaceHolder;
+import android.graphics.Canvas;
+
 
 /**
  * Created by bhaetsch on 25.05.2015.
@@ -9,10 +12,34 @@ public class MainThread extends Thread {
     private boolean running;
     private ArrayList<Spielobjekt> spielobjekte = new ArrayList<Spielobjekt>();
 
+    private SurfaceHolder surfaceHolder;
+    private MainGamePanel gamePanel;
+
     @Override
-    public void run(){
-        while(isRunning()) {
+    public void run() {
+        while (isRunning()) {
             //TODO
+
+            Canvas canvas;
+
+            while (running) {
+                canvas = null;
+                // try locking the canvas for exclusive pixel editing on the surface
+                try {
+                    canvas = this.surfaceHolder.lockCanvas();
+                    synchronized (surfaceHolder) {
+                        // update game state
+                        // draws the canvas on the panel
+                        this.gamePanel.onDraw(canvas); //kein Fehler!
+                    }
+                } finally {
+                    // in case of an exception the surface is not left in
+                    // an inconsistent state
+                    if (canvas != null) {
+                        surfaceHolder.unlockCanvasAndPost(canvas);
+                    }
+                } // end finally
+            }
         }
     }
 
@@ -22,5 +49,13 @@ public class MainThread extends Thread {
 
     public void setRunning(boolean running) {
         this.running = running;
+    }
+
+    public MainThread(SurfaceHolder surfaceHolder, MainGamePanel gamePanel) {
+        super();
+        this.surfaceHolder = surfaceHolder;
+        this.gamePanel = gamePanel;
+
+
     }
 }
